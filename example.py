b329a381f0dbaef238b9e36c7103fd72bee80138
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-04-17 15:16:17 -0400

    cleanup

--- example.py
+++ example.py
@@ -2,17 +2,6 @@ from copipes import coroutine, pipeline, null
 from copipes.macros.pipe import pipe
 
 
-
-def make_pipe(fn):
-    new_fn = pipe(fn, remove=['make_pipe'])
-
-    @wraps(new_fn)
-    @coroutine
-    def wrapper(*args, **kws):
-        return new_fn(*args, **kws)
-
-    return wrapper
-
 @pipe
 def putStrLn():
     """doc"""
@@ -31,4 +20,4 @@ if __name__ == '__main__':
         putStrLn,
         replicate.params(3),
         putStrLn,
-        ).feed(range(10))
+    ).feed(range(10))
